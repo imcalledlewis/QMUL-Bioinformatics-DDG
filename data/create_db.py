e2321ce29531d9e2f6a524dc6feaa7ac1850d390
whatbee <gabe.bf@gmail.com>
2023-01-28 14:26:11 +0000

    about to fork

--- data/create_db.py
+++ data/create_db.py
@@ -1,23 +1,36 @@
 import pandas as pd
 import sqlite3
 import os
+import re
+fileIn = "gwas_trimmed4.tsv"
+fileOut = 'snps.db'
 
 path = os.path.dirname(os.path.abspath(__file__))       # sets path relative to current file
-filepath = os.path.join(path, "gwas_trimmed2.tsv")        
-
+filepath = os.path.join(path, fileIn)        
 df = pd.read_csv(filepath, sep='\t')
+
 colName=""              # Empty string that will be filled later
 for col in df.columns:
     colName+=(col)      # Add another name column name
     colName+=(", ")     # Add a comma separator 
 colName=colName[:-2]    # removes last ' ,'
-print(colName)
+# print(colName)
+
+#colName = re.sub(r'[\W]', '_', colName)  # Replaces special characters and whitespace with underscores
+
+colonPos = colName.find(':')
+
+if colonPos:
+    print("\nFUCK\n")
+    print (colName[colonPos-5:colonPos+5])
+else:
+    print("\nYAY\n")
 
 
-path = os.path.dirname(os.path.abspath(__file__))       # sets path relative to current file
-db = os.path.join(path, 'snps.db')                  
-conn = sqlite3.connect(db)                              # Opens (or creates) a db file
-cur = conn.cursor()                                     # Sets cursor
-cur.execute("CREATE TABLE SNP(" + colName + ")")
-res = cur.execute("SELECT name FROM sqlite_master")
-print(res.fetchone())
+# path = os.path.dirname(os.path.abspath(__file__))       # sets path relative to current file
+# filepath = os.path.join(path, fileOut)                  
+# conn = sqlite3.connect(filepath)                        # Opens (or creates) a db file
+# cur = conn.cursor()                                     # Sets cursor
+# cur.execute("CREATE TABLE SNP(" + colName + ")")
+# res = cur.execute("SELECT name FROM sqlite_master")
+# print(res.fetchone())
--- data/gwas_clean.py
+++ data/gwas_clean.py
@@ -2,23 +2,23 @@ import pandas as pd
 import os
 import re
 
+fileIn = 'gwas_catalog_v1.0-associations_e108_r2023-01-14.tsv'
+fileOut = 'gwas_trimmed4.tsv'
+
 # https://www.ebi.ac.uk/gwas/docs/file-downloads
 
-
-
-path = os.path.dirname(os.path.abspath(__file__))       # sets path relative to current file
-filepath = os.path.join(path, 'gwas_catalog_v1.0-associations_e108_r2023-01-14.tsv')
+path = os.path.dirname(os.path.abspath(__file__))           # Sets path relative to current file
+filepath = os.path.join(path, fileIn)
 data = pd.read_csv(filepath, sep='\t', low_memory=False)
-data = data.loc[data['DISEASE/TRAIT']=='Type 1 diabetes']   # select only rows regarding type 1 diabetes
-data = data.loc[data['CHR_ID']=='6']                        # select only rows for chromosome 6
-#print(data.head())
+data = data.loc[data['DISEASE/TRAIT']=='Type 1 diabetes']   # Select only rows regarding type 1 diabetes
+data = data.loc[data['CHR_ID']=='6']                        # Select only rows for chromosome 6
 
-rename_dict={}                      # Empty dictionary to be filled later
+rename_dict={}                          # Empty dictionary to be filled later
 for col in data.columns:
-    newCol=re.sub(" ", "_", col)    # Column name with space replaced by underscore
+    newCol = re.sub(r'[\W]', '_', col)  # Replaces special characters and whitespace with underscores
     rename_dict.update({col:newCol})
 data=data.rename(columns=rename_dict)
 
-filepath = os.path.join(path, 'gwas_trimmed2.tsv')
+filepath = os.path.join(path, fileOut)
 data.to_csv(filepath, sep='\t')
 print("\ndone")
