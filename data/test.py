8e6a2490dc0d2e87bf30148f851b3d0156189af6
whatbee <gabe.bf@gmail.com>
2023-02-02 13:04:53 +0000

    Added data cleaner notebook

--- data/test.py
+++ data/test.py
@@ -6,4 +6,9 @@ import json
 filepath=getPath('Func_data.tsv')
 data=pd.read_csv(filepath,sep='\t')
 
-print(data) # multiple duplicate rs values, need to decide way to break ties
+# print(data) # multiple duplicate rs values, need to decide way to break ties
+dupeList = data.duplicated(subset=['#Uploaded_variation',],keep=False)   # Get list of duplicate values
+clear()
+# print(data[dupeList])
+print("")
+print(data[~dupeList])
