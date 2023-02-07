a2da2fd1c65b705199586ff5161b9982958f85f6
whatbee <gabe.bf@gmail.com>
2023-02-07 00:24:58 +0000

    :(

--- data/test.py
+++ data/test.py
@@ -1,10 +1,25 @@
 import requests
 from db_scripts import *
+clear()
 
-#rsids = ['rs1538171', 'rs4320356', 'rs1770','rs2647044','rs11755527','rs9388489','rs9268645','rs9272346','rs3757247','rs924043','rs1050979','rs9405661','rs12665429','rs212408','rs72928038','rs2045258','rs9273363','rs1578060','rs138748427','rs9273367','rs17711850']
+rsids = ['rs1538171', 'rs4320356', 'rs1770','rs2647044','rs11755527','rs9388489','rs9268645','rs9272346','rs3757247','rs924043','rs1050979','rs9405661','rs12665429','rs212408','rs72928038','rs2045258','rs9273363','rs1578060','rs138748427','rs9273367','rs17711850']
 
 fileIn = getPath('gwas_trimmed.tsv')
 fileOut = getPath('go_data_new.tsv')
+
+def get_gene_ontology(rsid):
+    url = "https://www.ebi.ac.uk/QuickGO/services/annotation/search"
+    params = {
+        "db": "rs",
+        "dbReference": rsid,
+        "taxonomy": "9606",
+        # "limit":"1"
+    }
+    response = requests.get(url, params=params)
+    assert response.status_code == 200, "unexpected response code"  # verify that response code is ok; throw error if not
+    data = response.json()
+    assert 'results' in data, "no results for "+rsid                # verify that results are found; throw error if not
+    return data['results']
 
 def get_go_term(GOid):
     url = f"https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/{GOid}"
@@ -12,7 +27,23 @@ def get_go_term(GOid):
     assert response.status_code == 200, "unexpected response code"  # verify that response code is ok; throw error if not
     data = response.json()
     assert 'results' in data, "no results for "+GOid                # verify that results are found; throw error if not
-    return data['results'][0]
+    return data['results'][0]                                       # return first row
 
-results=get_go_term("GO:0030170")
-print(results)
+
+dupesDict={}
+for rsid in rsids:
+    l=[]        # reset list each time
+    snpRes=get_gene_ontology(rsid)
+    for row in snpRes:
+        qualifier=row['qualifier']
+        goID=row['goId']
+        l.append(goID)
+
+    dupesDict.update({rsid:l})
+
+df=pd.DataFrame(dupesDict).T
+dupes=df.duplicated(keep=False)
+dupeNum= dupes.tolist().count(True)
+print(len(dupes), "columns, of which", dupeNum, "are duplicates")
+print (":(" if len(dupes) == dupeNum else ":)")
+
