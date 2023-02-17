5d686427f6ac75529ef94eab770f323a2109984d
Nasim <123819758+Nasim-MI@users.noreply.github.com>
2023-02-17 12:08:58 +0000

    Updated export_LD_results.py
    Fixed issue where some rows were ignored due to not checking for a SNP matching in both SNP_1 and SNP_2 columns of LD dataset

--- data/export_LD_results.py
+++ data/export_LD_results.py
@@ -14,9 +14,10 @@ def export_LD(SNP_list,LD_dataset_file):
     # Load LD dataset and create empty dataframe for filtered results
     LD_df = pd.read_table(LD_dataset_file)
     LD_results_df = pd.DataFrame(columns=['SNP_1', 'SNP_2', 'FIN_D\'', 'FIN_r2', 'TSI_D\'', 'TSI_r2 ', 'GBR_D\'', 'GBR_r2 ']) 
-    # Loop indexing LD dataset using each pair of SNPs and adding rows to results dataframe
+    # Loop indexing LD dataset using each pair of SNPs
     for SNP_1,SNP_2 in zip(SNP_1_list,SNP_2_list):
-        LD_row = LD_df.loc[(LD_df['SNP_1'] == SNP_1) & (LD_df['SNP_2'] == SNP_2)]
+        LD_row = LD_df.loc[((LD_df['SNP_1'] == SNP_1) & (LD_df['SNP_2'] == SNP_2) | 
+                            (LD_df['SNP_1'] == SNP_2) & (LD_df['SNP_2'] == SNP_1))] 
         LD_results_df = pd.concat([LD_results_df, LD_row])
     return LD_results_df
     
