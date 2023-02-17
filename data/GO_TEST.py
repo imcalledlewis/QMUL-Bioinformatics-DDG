b9116d580b8da46a5e12bd1d7be036d5d15be6e4
imcalledlewis <103501240+imcalledlewis@users.noreply.github.com>
2023-02-17 13:59:13 +0000

    Update GO_TEST.py
    Now works for location, Gene symbol and gene name

--- data/GO_TEST.py
+++ data/GO_TEST.py
@@ -9,20 +9,34 @@ app = Flask(__name__)
 @app.route('/')
 def index():
         return 'Welcome to Team Duck'
-    
-@app.route('/GENE_NAME/<gene>')
-def display_table(gene):
+# define a routed called GENE_SYM that accepts a Gene symbol name parameter   
+@app.route('/GENE_SYM/<gene>')
+def display_table_SYM(gene):
     #Read the CSV file
     data = pd.read_csv('GO_trimmed.csv')
     #Filter the data to only include rows where the 'SYMBOL' column matches the input gene
     filtered_data = data.loc[data['SYMBOL'] == gene]
     #Pass the filtered data to the 'table.html' template
     return render_template('table.html', data=filtered_data)
-# define a routed called 'SNP' that accepts a SNP name parameter
 
-    #except:
-        # if protein is not found a key error is thrown and we end up here
-        #return "We don't have any functional information about a SNP called %s." % Uploaded_variation
+# define a routed called 'SNP' that accepts a SNP name parameter
+@app.route('/GENE_NAME/<gene>')
+def display_table_NAME(gene):
+    #Read the CSV file
+    data = pd.read_csv('GO_trimmed.csv')
+    #Filter the data to only include rows where the 'SYMBOL' column matches the input gene
+    filtered_data = data.loc[data['Gene'] == gene]
+    #Pass the filtered data to the 'table.html' template
+    return render_template('table.html', data=filtered_data)
+    
+@app.route('/GENE_LOC/<Loc>')
+def display_table_LOC(Loc):
+    #Read the CSV file
+    data = pd.read_csv('GO_trimmed.csv')
+    #Filter the data to only include rows where the 'SYMBOL' column matches the input gene
+    filtered_data = data.loc[data['Location'] == Loc]
+    #Pass the filtered data to the 'table.html' template
+    return render_template('table.html', data=filtered_data)
 
 #start the server
 if __name__ == '__main__' :
