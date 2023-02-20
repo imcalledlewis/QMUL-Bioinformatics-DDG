0e3e9a5a76bfc63e94e194a04315ba9e51eccca9
Nadia Choudhury <123477898+NadiaChoudhury1995@users.noreply.github.com>
2023-02-20 22:25:48 +0000

    Manhattan plot update

--- data/Manplot.py
+++ data/Manplot.py
@@ -1,52 +1,49 @@
-# Importing required functions 
+# Importing required functions
 import numpy as np
-import seaborn as sns
 import pandas as pd
+import seaborn as sns
+import geneview as gv
+import base64
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('agg')
 from matplotlib.figure import Figure
 import io
 from io import BytesIO
+import urllib
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from flask import Flask, render_template, send_file, Response
+from flask import Flask, render_template, send_file
 
-df = pd.read_csv('.\TSVs\T1D_GWAS_add.tsv', sep='\t')
-fig,ax=plt.subplots(figsize=(10,8))
+fig,ax=plt.subplots(figsize=(8,8))
 ax=sns.set(style="darkgrid")
+
+df = pd.read_csv('./TSVs/T1D_GWAS_add.tsv', sep='\t')
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('graph.html')
+def index():
+    return 'Home'
 
-@app.route('/graph', methods=['GET','POST'])
-def graph():
-    my_data = df.sample(642) # for whole genome this number will be 10000, or however many you want to see
+@app.route('/plot')
+def plot():
     g=sns.relplot(
-        data=my_data,
-        x= 'cumulative_pos',
-        y= '-logp',
-        aspect = 4, # size of graph
-        hue = 'CHR_ID', # for whole genome this is the chromosome
-        palette= ['grey','black'] * 11, #random colour scheme, can be changed to any colour(s)
-        linewidth=0,
-        size=4,
-        legend=None
-        )
-    g.ax.set_xlabel('Chromosome') # x-axis label
-    g.ax.set_xticks(df.groupby('CHR_ID')['cumulative_pos'].median())
-    g.ax.set_xticklabels(df['CHR_ID'].unique())
-    g.fig.suptitle('Manhatton Plot showing Association between SNPs and T1DM in GWAS')
-    #annotations = my_data[my_data['-logp'] > 20].apply(lambda p : g.ax.annotate(p['SNPS'], (p['cumulative_pos'], p['-logp'])), axis=1).to_list()
-    #adjust_text(annotations, arrowprops = {'arrowstyle': '->', 'color':'blue'})
-
+    data = df.sample(642), # for whole genome this number will be 10000, or however many you want to see
+    x= 'cumulative_pos',
+    y= '-logp',
+    aspect = 4, # size of graph
+    hue = 'CHR_ID', # for whole genome this is the chromosome
+    palette= ['grey','black'] * 11, #random colour scheme, can be changed to any colour(s)
+    linewidth=0,
+    size=4,
+    legend=None
+    )
     canvas=FigureCanvas(fig)
     img=io.BytesIO()
-    fig.savefig(img)
+    fig.savefig(img, format='png')
     img.seek(0)
-
-    return send_file(img,mimetype='img/png')
-    
+    plot_data = urllib.parse.quote(base64.b64encode(img.getvalue()).decode('utf-8'))
+    return render_template('plot.html', plot_url=plot_data)
 
 
 # Main Driver Function 
