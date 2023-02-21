ace4dfbb759856c3250acd8c03cd6f309c1662c6
Nadia Choudhury <123477898+NadiaChoudhury1995@users.noreply.github.com>
2023-02-21 03:54:27 +0000

    Manhattan Plot Flask done

--- data/Manplot.py
+++ data/Manplot.py
@@ -1,50 +1,49 @@
-# Importing required functions
-import numpy as np
 import pandas as pd
-import seaborn as sns
-import geneview as gv
-import base64
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('agg')
-from matplotlib.figure import Figure
-import io
-from io import BytesIO
-import urllib
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from flask import Flask, render_template, send_file
+from bokeh.io import push_notebook, show, output_notebook
+from bokeh.layouts import row 
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.transform import linear_cmap
+import numpy as np
+from flask import Flask, request, render_template, abort, Response
 
-fig,ax=plt.subplots(figsize=(8,8))
-ax=sns.set(style="darkgrid")
-
-df = pd.read_csv('./TSVs/T1D_GWAS_add.tsv', sep='\t')
 
 app = Flask(__name__)
 
+#df = pd.read_csv('.\data\TSVs\T1D_GWAS_add.tsv', sep='\t')
+#df.CHR_ID.unique()
+#index_cmap = linear_cmap('CHR_ID', palette = ['grey','black'],low=1,high=22)
 @app.route('/')
 def index():
-    return 'Home'
+    return "Hello Team DuckDuck Go"
 
 @app.route('/plot')
 def plot():
-    g=sns.relplot(
-    data = df.sample(642), # for whole genome this number will be 10000, or however many you want to see
-    x= 'cumulative_pos',
-    y= '-logp',
-    aspect = 4, # size of graph
-    hue = 'CHR_ID', # for whole genome this is the chromosome
-    palette= ['grey','black'] * 11, #random colour scheme, can be changed to any colour(s)
-    linewidth=0,
-    size=4,
-    legend=None
-    )
-    canvas=FigureCanvas(fig)
-    img=io.BytesIO()
-    fig.savefig(img, format='png')
-    img.seek(0)
-    plot_data = urllib.parse.quote(base64.b64encode(img.getvalue()).decode('utf-8'))
-    return render_template('plot.html', plot_url=plot_data)
+    df = pd.read_csv('.\data\TSVs\T1D_GWAS_add.tsv', sep='\t')
+    df.CHR_ID.unique()
+    index_cmap = linear_cmap('CHR_ID', palette = ['grey','black'],low=1,high=22)
 
+    p=figure(plot_width=900, 
+         plot_height=500, 
+         title = "Manhattan Plot", 
+         toolbar_location=None, 
+         tools="hover", 
+         tooltips="@SNPS: (@CHR_ID,@CHR_POS)"
+         )
+    p.circle('cumulative_pos','-logp',
+          source=df, 
+          fill_alpha=0.6,
+          fill_color=index_cmap,
+          size=10, legend='CHR_ID'
+        )
+    p.xaxis.axis_label= 'Chromosome'
+    p.yaxis.axis_label= '-logp'
+    p.legend.location = "top_left"
+
+    script, div = components(p)
+   # cdn_js=CDN.js_files
+   # cdn_css=CDN.css_files
+    return render_template("Manplot.html", script=script, div=div,)
 
 # Main Driver Function 
 if __name__ == '__main__':
