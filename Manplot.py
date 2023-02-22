f566772e22c158a7be1fdcbfd3de7fc075491ca2
Nadia Choudhury <123477898+NadiaChoudhury1995@users.noreply.github.com>
2023-02-22 01:31:14 +0000

    Done updating Manhattan Plot

--- Manplot.py
+++ Manplot.py
@@ -22,7 +22,7 @@ def plot():
     index_cmap = linear_cmap('CHR_ID', palette = ['grey','black']*11,low=1,high=22)
     # Format figure
     p=figure(frame_width= 800, # graph size
-         plot_height=400, # graph size
+         plot_height=500, # graph size
          title = "Hover over a plot to see the SNP ID and chomosomal position", # Title added in html
          toolbar_location=None, 
          tools="hover", # Allows mouse hover to bring up information
@@ -42,10 +42,10 @@ def plot():
     # Correct the x-axis by replacing the cumulative_pos with the Chromosome position
     p.xaxis.ticker = [119895261, 373943002, 537393504, 716119012, 834845071, 964538826.5, 1147957441, 1306654016, 
     1396415411, 1540730893, 1674222993, 1823778230, 1930583064, 2055942929, 2141945578, 2202426536, 2302961360, 2388903150, 
-    2436333506.5, 2453458246, 2551821399, 2584586808]
+    2436333506.5, 2482024730, 2529377491, 2584586808]
     p.xaxis.major_label_overrides = {119895261:'1', 373943002:'2', 537393504: '3', 716119012:'4', 834845071:'5', 964538826.5:'6', 1147957441: '7', 1306654016: '8', 
     1396415411:'9', 1540730893:'10', 1674222993:'11', 1823778230:'12', 1930583064:'13', 2055942929:'14', 2088496163 :'14', 2141945578:'15',
-    2202426536:'16', 2302961360:'17', 2388903150:'18', 2436333506.5:'19', 2453458246:'20', 2551821399:'21', 2584586808: '22'}
+    2202426536:'16', 2302961360:'17', 2388903150:'18', 2436333506.5:'19', 2482024730:'20', 2529377491:'21', 2584586808: '22'}
     script, div = components(p)
  
     return render_template("Manplot.html", script=script, div=div,)
