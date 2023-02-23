7ca2c2595f45d78bb1c93426890720770cc0e7c8
whatbee <gabe.bf@gmail.com>
2023-02-23 11:19:14 +0000

    add auto detect request type

--- flask-app.py
+++ flask-app.py
@@ -22,7 +22,7 @@ app.config['SECRET_KEY'] = 'change this unsecure key'   # TODO: read about this 
 # create a class to define the form
 class QueryForm(FlaskForm):
 	SNP_req = StringField('Enter SNP information: ', validators=[InputRequired()])
-	req_type = SelectField("Information type: ", choices=[('rsid', "SNP name(s) (rs value)"), ("coords","genomic coordinates"), ("geneName","gene name")])
+	req_type = SelectField("Information type: ", choices=[('auto', 'Automatically detect'),('rsid', "SNP name(s) (rs value)"), ("coords","genomic coordinates"), ("geneName","gene name")])
 	submit = SubmitField('Submit')
 
 
@@ -53,6 +53,17 @@ def SNP(SNP_req):
 	req_type=request.args.get('req_type',default="empty_req_type")	# Gets type of information inputted (the bit after "?")
 	assert req_type != "empty_req_type", "request type is empty"
 
+	if req_type == 'auto':
+		if re.search(r'rs\d+',SNP_req):
+			req_type='rsid'
+		elif re.search(r'\d:\d+', SNP_req):
+			req_type='coords'
+		elif re.search(r'\w{1,10}', SNP_req):
+			req_type='geneName'
+		else:
+			# raise()
+			pass
+
 	SNP_req = SNP_req.lower()		# Ensure snp name is in lowercase letters
 	name=SNP_req
 	reqRes,SNP_list=DBreq(SNP_req, req_type)	# Make SQL request
@@ -65,7 +76,7 @@ def SNP(SNP_req):
 				name=str(list(reqRes.keys())[0])
 			else:
 				name=f'{l} SNPS	'
-			return render_template('view.html', reqRes=reqRes, name=name, req_type=req_type, len = len(SNP_list), SNP_req = SNP_req,debug=debug)
+			return render_template('view.html', reqRes=reqRes, name=name, req_type=req_type, len=len(SNP_list), SNP_req=SNP_req,debug=debug)
 	else:                 			# If SNP is not found:
 		return render_template('not_found.html', name=name)
 
