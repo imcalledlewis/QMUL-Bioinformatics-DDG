7e2c8a1a4519df0a756f9f503598466d033b9cb5
whatbee <gabe.bf@gmail.com>
2023-03-01 01:47:31 +0000

    fix coord request

--- data/db_scripts.py
+++ data/db_scripts.py
@@ -40,16 +40,9 @@ def DBreq(request, request_type, manPlot=False):       # Makes SQL request
         request=request.split(',')		    # Split request by comma separator
 
     elif request_type=="coords":            # co-ordinate (6:1234-6:5678) search
-        if '-' not in request:              # If there's only one coord,
-            request=(request+"-"+request)   # pretend it's a range and the start/ stop are the same
-        request=request.split('-')		    # Split request by hyphen separator
-        assert len(request)==2, "Too many coordinate inputs"
-        coords_chr=[i.split(':')[0] for i in request]   # Gets the chromosome from each coord
-        coords_loc=[i.split(':')[1] for i in request]   # Gets the location from each coord
-        assert len(coords_chr)==2, "Can't understand co-ord input"
-        assert coords_chr[0]==coords_chr[1], "Unequal chromosome input"
-
-        req=(coords_chr[0],coords_loc[0],coords_loc[1])
+        coord1,coord2=request.split('-')    # Split request by hyphen separator
+        chr,coord1=coord1.split(':')        # Get chromosome by splitting by colon
+        req=(chr,coord1,coord2)
         res = cur.execute("SELECT rsid FROM gwas WHERE chr_id LIKE ? AND chr_pos BETWEEN ? AND ?",req)
         ret=res.fetchall()
         request=[i[0] for i in ret]         # SQL request returns list of singleton tuples, this line converts them to flat list
