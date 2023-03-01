157f8ede8e6b9660687552a91566c6ba417bf68f
whatbee <gabe.bf@gmail.com>
2023-03-01 15:21:07 +0000

    update documentation pdf

--- Documentation/code_snippets/SQL/pdDB.py
+++ Documentation/code_snippets/SQL/pdDB.py
@@ -10,13 +10,12 @@ def pdDB(tsv_path,table_name,dtype): # Adds tsv to SQL database
     df = pd.read_csv(tsv_path, sep='\t')
     df.to_sql(name=table_name, con=conn, index=False, dtype=dtype)
 
-
 def getPath(file,tsv=None): # Returns the absolute path to a file that is in same folder as script
-    filenames=('tsv', 'csv', 'txt') # List of table storing files
+    filenames={'tsv', 'csv', 'txt'} # Set of table storing files
     ext=file.split('.') # Splits by '.'
     ext=ext[-1].lower() # Uses this information to get extension of file
     path = os.path.dirname(os.path.abspath(__file__)) # Gets current path of file
-    if (tsv==True) or (tsv==None and any([x == ext for x in filenames])): # If it's a table storing file:
+    if (tsv==True) or (tsv==None and ext in filenames): # If it's a table storing file:
             filepath = os.path.join(path,"TSVs",file) # Sets path relative to current file, inside 'TSVs' folder
     else:
             filepath = os.path.join(path,file) # Sets path relative to current file
