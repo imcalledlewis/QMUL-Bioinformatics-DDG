b660d99c5c785c49270e1e697fc23469dd7a63b0
whatbee <gabe.bf@gmail.com>
2023-03-02 16:25:25 +0000

    remove latex documentation

--- Documentation/code_snippets/LD/heatmap1.py
+++
@@ -1,6 +0,0 @@
-# load LD dataset
-LD_df = pd.read_table('LD_T1DM_Chr6.tsv')
-# checks for SNPs in subset which are not in LD dataset
-SNP_list = remove_invalid_SNPs(SNP_list)
-# create a pair of lists containing the 1st and 2nd SNP of each combination
-SNP_1_list, SNP_2_list = SNP_pair_lists(SNP_list)
